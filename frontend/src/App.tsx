e7bd95ecb3e0969a4dd8e6e456f0443c642b4837
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-05-28 13:52:14 -0700

    fronend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <header>
+          <a href="/"> Omozon </a>
+        </header>
+        <main>
+          list products
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
