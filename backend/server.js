370119e13051491edd772ea02ccd49e6ceb2741c
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-05-29 13:19:49 -0700

    use reducer

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+// Enable CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Handle GET request for /api/products
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
--- frontend/src/pages/HomeScreen.tsx
+++ frontend/src/pages/HomeScreen.tsx
@@ -1,37 +1,85 @@
-import data from "../api/data"
-import { Link } from "react-router-dom"
+import { useEffect, useReducer } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
 
-const HomeScreen = () =>{
-
-    return (
-        <div>
-            <h1>Featured Products</h1>
-          <div className='products'>
-          {
-            data.products.map(product =>(
-              <div className='product' key={product.slug}>
-                <Link href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className='product-info'>
-                  <Link href={`/product/${product.slug}`}>
-                    <p>
-                      {product.name}
-                    </p>
-                  </Link>
-                  <p>
-                    <strong>
-                      {product.prices}
-                    </strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))
-          }
-          </div>
-        </div>
-    )
+interface Product {
+  slug: string;
+  name: string;
+  image: string;
+  prices: number;
 }
 
-export default HomeScreen
+interface State {
+  products: Product[];
+  loading: boolean;
+  error: any;
+}
+
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+const initialState: State = {
+  products: [],
+  loading: true,
+  error: [],
+};
+
+const HomeScreen = () => {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get<Product[]>('http://localhost:5000/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+      }
+      // setProducts(result.data)
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className='products'>
+        {products.map((product) => (
+          <div className='product' key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className='product-info'>
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>{product.prices}</strong>
+              </p>
+              <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
