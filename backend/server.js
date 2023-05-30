ea502bc4aef96c9057a4dabca9571a035a41884d
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-05-29 23:58:48 -0700

    final

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,15 @@ app.get('/api/products/:id', (req, res) => {
   }
 });
 
+// const __dirname = path.resolve()
+// app.use(express.static(path.join(__dirname, '/frontend/build')))
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
+//   )
+
+app.use((err, req, res, next) =>{
+  res.status(500).send({message: err.message})
+})
 
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
