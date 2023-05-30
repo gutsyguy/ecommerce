903cc09ef342dfeaead5f9894a6690b6e22cdb73
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-05-30 11:10:15 -0700

    convert backend to common js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import data from './data.js';
+const express = require('express')
+const data = require('./data.js')
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -37,11 +37,11 @@ app.get('/api/products/:id', (req, res) => {
   }
 });
 
-// const __dirname = path.resolve()
-// app.use(express.static(path.join(__dirname, '/frontend/build')))
-// app.get('*', (req, res) =>
-//     res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
-//   )
+//Server production
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join('frontend/dist')))
+  app.get("*",(req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build' , 'index.html')))
+}
 
 app.use((err, req, res, next) =>{
   res.status(500).send({message: err.message})
