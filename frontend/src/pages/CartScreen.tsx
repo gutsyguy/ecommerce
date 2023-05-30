182ff31c4a79dea2edd43dcbe54e341aea297cca
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-05-29 23:13:53 -0700

    cart screen finished

--- frontend/src/pages/CartScreen.tsx
+++ frontend/src/pages/CartScreen.tsx
@@ -1,9 +1,10 @@
 import { useContext } from "react"
 import { Store } from "../Store"
 import { Helmet } from "react-helmet-async"
-import { Row, Col, ListGroup, Button } from "react-bootstrap"
+import { Row, Col, ListGroup, Button, Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import MessageBox from "../components/MessageBox"
+
 
 const CartScreen = () => {
     const {state, dispatch: ctxDispatch} = useContext(Store)
@@ -54,7 +55,30 @@ const CartScreen = () => {
                 )
                 }
             </Col>
-            <Col md={4}></Col>
+            <Col md={4}>
+                <Card>
+                    <Card.Body>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <h3>
+                                    Subtotal ({cartItems.reduce((a:any, c:any) => a + c.quantity,0)}{' '} items):
+                                    ${cartItems.reduce((a:any,c:any) => a + c.price * c.quantity, 0)}
+                                </h3>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <div className="d-grid">
+                                    <Button
+                                        type="button"
+                                        variant='primary'
+                                        disabled={cartItems.length == 0}>
+                                            Proceed to Checkout
+                                    </Button>
+                                </div>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>
+            </Col>
         </Row>
     </div>
   )
