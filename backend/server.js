2da106e3d8a6fe1c08fbd6ec5bccd5e3c4439a28
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-05-30 11:27:13 -0700

    revert

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 // middleware
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-ewipdamn7-gutsyguy.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
